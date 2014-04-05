c5165b4419ed9eb458f773508c2417b245ef2e8d
Danila <daniladanila2378@gmail.com>
2014-04-05 04:34:00 +0300

    JSON

--- static/js/parser.js
+++ static/js/parser.js
@@ -2,8 +2,8 @@ var parser = {};
 
 parser.createRowEventTable = function(JSON){
 		//var obj = jQuery.parseJSON( '{ "name": "John", "danila":"romaniuk" }' );
-	var obj = jQuery.parseJSON(JSON);
-	$('#eventsTable tr:last').after("<tr><td>" + obj.typeOfSport + "</td><td>" + obj.date + "</td><td><a href='people_list.html' target='_blank'><span class='glyphicon glyphicon-eye-open'></span></a></td></tr>");
+	//var obj = jQuery.parseJSON(JSON);
+	$('#eventsTable tr:last').after("<tr><td>" + JSON.typeOfSport + "</td><td>" + JSON.date + "</td><td><a href='people_list.html' target='_blank'><span class='glyphicon glyphicon-eye-open'></span></a></td></tr>");
 }
 
 parser.createRowPeople = function(JSON){
